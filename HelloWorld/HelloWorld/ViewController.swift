44c60b4ff2204d03bc0107c493a28aab3d4ff54d
Jeonhui <whas02@naver.com>
2022-01-12 19:21:17 +0900

    +
    +

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 이전희 on 2022/01/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var lblHello: UILabel!
+    @IBOutlet var lblSwift: UILabel!
+    @IBOutlet var lblName: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello, "+txtName.text!
+    }
+    
+}
+
