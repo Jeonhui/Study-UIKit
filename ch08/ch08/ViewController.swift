5129d3082d176163621826ef798a0900249cc549
Jeonhui <whas02@naver.com>
2022-01-15 20:43:28 +0900

    +
    +

--- ch08/ch08/ViewController.swift
+++ ch08/ch08/ViewController.swift
@@ -22,22 +22,32 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         lblLocationInfo2.text = ""
         locationManger.delegate = self
         locationManger.desiredAccuracy = kCLLocationAccuracyBest
+        locationManger.requestWhenInUseAuthorization()
         locationManger.startUpdatingLocation()
         myMap.showsUserLocation = true
     }
     
     
-    func goLocation(latitudeValue:CLLocationDegrees, longitudeValue:CLLocationDegrees, delta span :Double){
+    func goLocation(latitudeValue:CLLocationDegrees, longitudeValue:CLLocationDegrees, delta span :Double)->CLLocationCoordinate2D{
         let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
         let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
         let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
         myMap.setRegion(pRegion, animated: true)
+        return pLocation
+    }
+    
+    func setAnnotation(latitudeValue:CLLocationDegrees, longitudeValue:CLLocationDegrees, delta span:Double, title strTitle:String, subtitle strSubtitle:String){
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = goLocation(latitudeValue: latitudeValue, longitudeValue: longitudeValue, delta: span)
+        annotation.title = strTitle
+        annotation.subtitle = strSubtitle
+        myMap.addAnnotation(annotation)
     }
     
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let pLocation  = locations.last
-        goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
+        _ = goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
         CLGeocoder().reverseGeocodeLocation(pLocation!,completionHandler: {
             (placemarks, error)->Void in
             let pm = placemarks!.first
@@ -59,6 +69,21 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0{
+            self.lblLocationInfo1.text = ""
+            self.lblLocationInfo2.text = ""
+            locationManger.startUpdatingLocation()
+
+        }else if sender.selectedSegmentIndex == 1{
+            setAnnotation(latitudeValue: 37.33739753631028, longitudeValue: 127.26574656604181, delta: 0.1, title: "한국외대 글로벌", subtitle: "경기도 용인시 처인구 모현읍 외대로 81")
+            self.lblLocationInfo1.text = "보고 계신 위치"
+            self.lblLocationInfo2.text = "김포고등학교"
+        }else if sender.selectedSegmentIndex == 2{
+            setAnnotation(latitudeValue: 37.61856537391298, longitudeValue: 126.71250125599356, delta: 0.1, title: "김포고등학교", subtitle: "경기도 김포시 봉화로 51번길")
+            self.lblLocationInfo1.text = "보고 계신 위치"
+            self.lblLocationInfo2.text = "김포고등학교"
+            
+        }
         
     }
     
