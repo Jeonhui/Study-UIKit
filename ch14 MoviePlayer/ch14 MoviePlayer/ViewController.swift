2a183547d33a4edd5316e9b8b229e9ed65af6cc2
Jeonhui <whas02@naver.com>
2022-01-17 17:57:22 +0900

    +

---
+++ ch14 MoviePlayer/ch14 MoviePlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ch14 MoviePlayer
+//
+//  Created by 이전희 on 2022/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
