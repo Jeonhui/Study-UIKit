5c101e4735bded1944433b3e38691e5b85ad6e49
Jeonhui <whas02@naver.com>
2022-01-25 17:49:18 +0900

    +

---
+++ study01/study01/LoginView.swift
@@ -0,0 +1,125 @@
+//
+//  ContentView.swift
+//  study01
+//
+//  Created by 이전희 on 2022/01/25.
+//
+
+import SwiftUI
+import Firebase
+
+class FirebaseManager : NSObject{
+    let auth: Auth
+    static let shared = FirebaseManager()
+    
+    override init(){
+        FirebaseApp.configure()
+        self.auth = Auth.auth()
+        super.init()
+    }
+}
+
+struct LoginView: View {
+    
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    var github:UIImage? = UIImage(named: "github_logo.png")
+    
+    var body: some View {
+        NavigationView{
+            ScrollView{
+                
+                VStack(spacing:16){
+                    Picker(selection: $isLoginMode, label: Text("Picker here")){
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                    
+                    if !isLoginMode{
+                        Button{
+                            
+                        } label: {
+                            Image(systemName: "person.fill")
+                                .font(.system(size:64))
+                                .padding()
+                        }
+                    }
+                    
+                    Group{
+                        TextField("Email", text: $email).keyboardType(.emailAddress)
+                        SecureField("Password", text: $password)
+                            .autocapitalization(.none)
+                    }                            .padding()
+                        .background(Color.white)
+                    
+                    Button{
+                        handleAction()
+                    }label:{
+                        HStack{
+                            Spacer()
+                            Text(isLoginMode ? "Log In" : "Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical,10)
+                                .font(.system(size:14,weight: .semibold))
+                            Spacer()
+                        }.background(Color.blue)
+                    }.padding()
+                    Text(self.loginStatusMessage).foregroundColor(self.loginStatusMessage.hasPrefix("Successfully") ? .green : .red)
+                }
+                .padding()
+                
+            }
+            .navigationTitle(isLoginMode ? "Login":"Create Account")
+            .background(Color(.init(white: 0,alpha: 0.05)).ignoresSafeArea())
+        }.navigationViewStyle(StackNavigationViewStyle())
+    }
+    
+    
+    
+    private func handleAction(){
+        if isLoginMode{
+            //print("should log into Firebase with existing credentials")
+            loginUser()
+        }else{
+            createNewAccount()
+            print("Resister a new account insize of Firebase Auth and then store image in Storage somhow ...")
+        }
+    }
+    private func loginUser(){
+        FirebaseManager.shared.auth.signIn(withEmail: email, password: password){
+            result, err in
+            if let err = err{
+                print("Failed to login user: ",err)
+                self.loginStatusMessage = "Failed to login user: \(err)"
+                return
+            }
+            print("Successfully logged in user: \(result?.user.uid ?? "")")
+            self.loginStatusMessage = "Successfully logged in user: \(result?.user.uid ?? "")"
+        }
+    }
+    
+    @State var loginStatusMessage = ""
+    
+    private func createNewAccount(){
+        FirebaseManager.shared.auth.createUser(withEmail: email, password: password){
+            result, err in
+            if let err = err{
+                print("Failed to create user: ",err)
+                self.loginStatusMessage = "Failed to create user: \(err)"
+                return
+            }
+            print("Successfully created user: \(result?.user.uid ?? "")")
+            self.loginStatusMessage = "Successfully created user: \(result?.user.uid ?? "")"
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ study01/study01/study01App.swift
@@ -0,0 +1,17 @@
+//
+//  study01App.swift
+//  study01
+//
+//  Created by 이전희 on 2022/01/25.
+//
+
+import SwiftUI
+
+@main
+struct study01App: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
