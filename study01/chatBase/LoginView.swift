1feee3a6c3a79672d9f0f95bf74c69a770ed2f77
Jeonhui <whas02@naver.com>
2022-02-03 13:45:14 +0900

    +

--- study01/chatBase/LoginView.swift
+++ study01/chatBase/LoginView.swift
@@ -6,10 +6,11 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct LoginView: View {
     
-    let didCompleteLoginProcess:()->()
+    let didCompleteLoginProcess: () -> ()
     
     @State private var isLoginMode = false
     @State private var email = ""
@@ -18,173 +19,178 @@ struct LoginView: View {
     @State private var shouldShowImagePicker = false
     
     var body: some View {
-        NavigationView{
-            ScrollView{
-                VStack(spacing:16){
-                    Picker(selection: $isLoginMode, label: Text("Picker here")){
+        NavigationView {
+            ScrollView {
+                
+                VStack(spacing: 16) {
+                    Picker(selection: $isLoginMode, label: Text("Picker here")) {
                         Text("Login")
                             .tag(true)
                         Text("Create Account")
                             .tag(false)
                     }.pickerStyle(SegmentedPickerStyle())
-                    
-                    if !isLoginMode{
-                        Button{
-                            shouldShowImagePicker
-                                .toggle()
+                        
+                    if !isLoginMode {
+                        Button {
+                            shouldShowImagePicker.toggle()
                         } label: {
-                            VStack{
-                                if let image = self.image{
-                                    Image(uiImage:image)
+                            
+                            VStack {
+                                if let image = self.image {
+                                    Image(uiImage: image)
                                         .resizable()
                                         .scaledToFill()
                                         .frame(width: 128, height: 128)
                                         .cornerRadius(64)
-                                }else{
+                                } else {
                                     Image(systemName: "person.fill")
-                                        .font(.system(size:64))
+                                        .font(.system(size: 64))
                                         .padding()
                                         .foregroundColor(Color(.label))
                                 }
                             }
-                            .overlay(RoundedRectangle(cornerRadius: 64).stroke(Color.black,lineWidth: 3)
+                            .overlay(RoundedRectangle(cornerRadius: 64)
+                                        .stroke(Color.black, lineWidth: 3)
                             )
+                            
                         }
                     }
                     
-                    
-                    Group{
-                        TextField("Email", text: $email).keyboardType(.emailAddress)
+                    Group {
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
                             .autocapitalization(.none)
                         SecureField("Password", text: $password)
-                            .autocapitalization(.none)
-                        
-                    }.padding()
-                        .background(Color.white)
+                    }
+                    .padding(12)
+                    .background(Color.white)
                     
-                    
-                    Button{
+                    Button {
                         handleAction()
-                    }label:{
-                        HStack{
+                    } label: {
+                        HStack {
                             Spacer()
                             Text(isLoginMode ? "Log In" : "Create Account")
                                 .foregroundColor(.white)
-                                .padding(.vertical,10)
-                                .font(.system(size:14,weight: .semibold))
+                                .padding(.vertical, 10)
+                                .font(.system(size: 14, weight: .semibold))
                             Spacer()
                         }.background(Color.blue)
-                    }.padding()
-                    Text(self.loginStatusMessage).foregroundColor(self.loginStatusMessage.hasPrefix("Successfully") ? .green : .red)
+                        
+                    }
+                    
+                    Text(self.loginStatusMessage)
+                        .foregroundColor(.red)
                 }
                 .padding()
                 
             }
-            .navigationTitle(isLoginMode ? "Login":"Create Account")
-            .background(Color(.init(white: 0,alpha: 0.05)).ignoresSafeArea())
-        }.navigationViewStyle(StackNavigationViewStyle())
-            .fullScreenCover(isPresented: $shouldShowImagePicker, onDismiss: nil){
-                ImagePicker(image: $image)
-            }
+            .navigationTitle(isLoginMode ? "Log In" : "Create Account")
+            .background(Color(.init(white: 0, alpha: 0.05))
+                            .ignoresSafeArea())
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+        .fullScreenCover(isPresented: $shouldShowImagePicker, onDismiss: nil) {
+            ImagePicker(image: $image)
+                .ignoresSafeArea()
+        }
     }
     
     @State var image: UIImage?
     
-    
-    private func handleAction(){
-        if isLoginMode{
-            //print("should log into Firebase with existing credentials")
+    private func handleAction() {
+        if isLoginMode {
+//            print("Should log into Firebase with existing credentials")
             loginUser()
-        }else{
+        } else {
             createNewAccount()
-            print("Resister a new account insize of Firebase Auth and then store image in Storage somhow ...")
+//            print("Register a new account inside of Firebase Auth and then store image in Storage somehow....")
         }
     }
-    private func loginUser(){
-        FirebaseManager.shared.auth.signIn(withEmail: email, password: password){
-            result, err in
-            if let err = err{
-                print("Failed to login user: ",err)
+    
+    private func loginUser() {
+        FirebaseManager.shared.auth.signIn(withEmail: email, password: password) { result, err in
+            if let err = err {
+                print("Failed to login user:", err)
                 self.loginStatusMessage = "Failed to login user: \(err)"
                 return
             }
-            print("Successfully logged in user: \(result?.user.uid ?? "")")
-            self.loginStatusMessage = "Successfully logged in user: \(result?.user.uid ?? "")"
+            
+            print("Successfully logged in as user: \(result?.user.uid ?? "")")
+            
+            self.loginStatusMessage = "Successfully logged in as user: \(result?.user.uid ?? "")"
             
             self.didCompleteLoginProcess()
         }
     }
     
-    
     @State var loginStatusMessage = ""
     
-    private func createNewAccount(){
-        if self.image == nil{
+    private func createNewAccount() {
+        if self.image == nil {
             self.loginStatusMessage = "You must select an avatar image"
             return
         }
         
-        FirebaseManager.shared.auth.createUser(withEmail: email, password: password){
-            result, err in
-            if let err = err{
-                print("Failed to create user: ",err)
+        FirebaseManager.shared.auth.createUser(withEmail: email, password: password) { result, err in
+            if let err = err {
+                print("Failed to create user:", err)
                 self.loginStatusMessage = "Failed to create user: \(err)"
                 return
             }
+            
             print("Successfully created user: \(result?.user.uid ?? "")")
+            
             self.loginStatusMessage = "Successfully created user: \(result?.user.uid ?? "")"
+            
             self.persistImageToStorage()
         }
     }
-    private func persistImageToStorage(){
-        //let filename = UUID().uuidString
-        guard let uid = FirebaseManager.shared.auth.currentUser?.uid
-        else {return}
-        
+    
+    private func persistImageToStorage() {
+//        let filename = UUID().uuidString
+        guard let uid = FirebaseManager.shared.auth.currentUser?.uid else { return }
         let ref = FirebaseManager.shared.storage.reference(withPath: uid)
-        
-        guard let imageData = self.image?.jpegData(compressionQuality: 0.5)
-        else{return}
-        
-        ref.putData(imageData, metadata:nil){
-            metadata, err in
-            if let err = err{
+        guard let imageData = self.image?.jpegData(compressionQuality: 0.5) else { return }
+        ref.putData(imageData, metadata: nil) { metadata, err in
+            if let err = err {
                 self.loginStatusMessage = "Failed to push image to Storage: \(err)"
                 return
             }
-            ref.downloadURL{
-                url, err in
-                if let err = err{
-                    self.loginStatusMessage = "Failed to downloadURL: \(err)"
+            
+            ref.downloadURL { url, err in
+                if let err = err {
+                    self.loginStatusMessage = "Failed to retrieve downloadURL: \(err)"
                     return
                 }
+                
                 self.loginStatusMessage = "Successfully stored image with url: \(url?.absoluteString ?? "")"
+                
                 guard let url = url else { return }
-                storeUserInformation(url)
+                self.storeUserInformation(imageProfileUrl: url)
             }
         }
     }
-    private func storeUserInformation(_ imageProfileUrl: URL){
-        guard let uid = FirebaseManager.shared.auth.currentUser?.uid else{return}
-        let userData = ["email":self.email, "uid":uid,"profileImageUrl":imageProfileUrl.absoluteString]
+    
+    private func storeUserInformation(imageProfileUrl: URL) {
+        guard let uid = FirebaseManager.shared.auth.currentUser?.uid else { return }
+        let userData = ["email": self.email, "uid": uid, "profileImageUrl": imageProfileUrl.absoluteString]
         FirebaseManager.shared.firestore.collection("users")
-            .document(uid).setData(userData){
-                err in
-                if let err = err{
+            .document(uid).setData(userData) { err in
+                if let err = err {
                     print(err)
-                    self.loginStatusMessage="\(err)"
+                    self.loginStatusMessage = "\(err)"
                     return
                 }
+                
                 print("Success")
                 
                 self.didCompleteLoginProcess()
             }
     }
-    
 }
 
-
-struct ContentView_Previews: PreviewProvider {
+struct ContentView_Previews1: PreviewProvider {
     static var previews: some View {
         LoginView(didCompleteLoginProcess: {
             
--- study01/chatBase/MainMessages/MainMessagesView.swift
+++ study01/chatBase/MainMessages/MainMessagesView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 import SDWebImageSwiftUI
 
-
-
-class MainMessgesViewModel: ObservableObject{
+class MainMessagesViewModel: ObservableObject {
     
     @Published var errorMessage = ""
     @Published var chatUser: ChatUser?
     
-    init(){
+    init() {
+        
         DispatchQueue.main.async {
             self.isUserCurrentlyLoggedOut = FirebaseManager.shared.auth.currentUser?.uid == nil
         }
@@ -23,38 +22,27 @@ class MainMessgesViewModel: ObservableObject{
         fetchCurrentUser()
     }
     
-    func fetchCurrentUser(){
-        self.errorMessage="Fetching current user"
-        guard let uid =  FirebaseManager.shared.auth.currentUser?.uid else {
+    func fetchCurrentUser() {
+        guard let uid = FirebaseManager.shared.auth.currentUser?.uid else {
             self.errorMessage = "Could not find firebase uid"
-            return }
+            return
+        }
         
-        FirebaseManager.shared.firestore.collection("users")
-            .document(uid).getDocument{ snapshot, error in
-                if let error = error{
-                    self.errorMessage  = "Failed to fetch current user: \(error)"
-                    print("Failed to fetch current user: ", error)
-                    return
-                }
-                
-                //self.errorMessage = "123"
-                
-                guard let data = snapshot?.data() else {
-                    self.errorMessage = "No data found"
-                    return
-                }
-                
-                //                self.errorMessage = "Data: \(data.description)"
-                
-                self.chatUser = .init(data: data)
-                
-//                let uid = data["uid"] as? String ?? ""
-//                let email = data["email"] as? String ?? ""
-//                let profileImageUrl = data["profileImageUrl"] as? String ?? ""
-//                self.chatUser = ChatUser(data: data)
-                
-                //self.errorMessage = chatUser.profileImageUrl
+        FirebaseManager.shared.firestore.collection("users").document(uid).getDocument { snapshot, error in
+            if let error = error {
+                self.errorMessage = "Failed to fetch current user: \(error)"
+                print("Failed to fetch current user:", error)
+                return
             }
+            
+            guard let data = snapshot?.data() else {
+                self.errorMessage = "No data found"
+                return
+                
+            }
+            
+            self.chatUser = .init(data: data)
+        }
     }
     
     @Published var isUserCurrentlyLoggedOut = false
@@ -63,102 +51,101 @@ class MainMessgesViewModel: ObservableObject{
         isUserCurrentlyLoggedOut.toggle()
         try? FirebaseManager.shared.auth.signOut()
     }
+    
 }
 
 struct MainMessagesView: View {
     
-    @State var shouldShowLogOutOptions = false
-    @State var OnlineStatus = false
-    @ObservedObject private var vm = MainMessgesViewModel()
+    @State var shouldShowLogOutOptions = true //
+    
+    @ObservedObject private var vm = MainMessagesViewModel()
     
     var body: some View {
-        NavigationView{
-            VStack{
-                //Text("USER: \(vm.chatUser?.uid ?? "")")
+        NavigationView {
+            
+            VStack {
+//                Text("User: \(vm.chatUser?.uid ?? "")")
                 customNavBar
                 messagesView
-            }.overlay(
+            }
+            .overlay(
                 newMessageButton, alignment: .bottom)
-                .navigationBarHidden(true)
+            .navigationBarHidden(true)
         }
     }
     
-    private var customNavBar: some View{
-        //custom nav bar
-        HStack(spacing: 16){
+    private var customNavBar: some View {
+        HStack(spacing: 16) {
             
-            WebImage(url: URL(string: vm.chatUser?.profileImageUrl ?? "")).resizable()
+            WebImage(url: URL(string: vm.chatUser?.profileImageUrl ?? ""))
+                .resizable()
                 .scaledToFill()
-                .frame(width: 50,height: 50)
+                .frame(width: 50, height: 50)
                 .clipped()
                 .cornerRadius(50)
                 .overlay(RoundedRectangle(cornerRadius: 44)
                             .stroke(Color(.label), lineWidth: 1)
                 )
-                .shadow(radius:5)
+                .shadow(radius: 5)
             
-            //            Image(systemName: "person.fill")
-            //                .font(.system(size: 34, weight: .heavy))
-            VStack(alignment: .leading, spacing: 4){
-                let email = vm.chatUser?.email.replacingOccurrences(of: "@gmail.com", with: "") ?? "email"
+            
+            VStack(alignment: .leading, spacing: 4) {
+                let email = vm.chatUser?.email.replacingOccurrences(of: "@gmail.com", with: "") ?? ""
                 Text(email)
-                    .font(.system(size: 24,weight: .bold))
-                HStack{
-                    Button {
-                        OnlineStatus.toggle()
-                    } label: {
-                        Circle()
-                            .foregroundColor(OnlineStatus ? .green : Color(.lightGray))
-                            .frame(width:14, height: 14)
-                        Text(OnlineStatus ? "online" : "offline")
-                            .font(.system(size: 12))
-                            .foregroundColor(Color(.lightGray))
-                    }
+                    .font(.system(size: 24, weight: .bold))
+                
+                HStack {
+                    Circle()
+                        .foregroundColor(.green)
+                        .frame(width: 14, height: 14)
+                    Text("online")
+                        .font(.system(size: 12))
+                        .foregroundColor(Color(.lightGray))
                 }
+                
             }
+            
             Spacer()
-            Button{
+            Button {
                 shouldShowLogOutOptions.toggle()
             } label: {
                 Image(systemName: "gear")
-                    .font(.system(size: 24,weight: .bold))
+                    .font(.system(size: 24, weight: .bold))
                     .foregroundColor(Color(.label))
             }
-        }.padding()
-            .actionSheet(isPresented: $shouldShowLogOutOptions) {
-                .init(title: Text("Settings"),message: Text("What do you want to do?"), buttons: [
-                    .destructive(Text("Sign Out"), action: {
-                        print("handle sign out")
-                        vm.handleSignOut() // signOut
-                    }),
-                    //.default(Text("DEFAULT BUTTON")),
+        }
+        .padding()
+        .actionSheet(isPresented: $shouldShowLogOutOptions) {
+            .init(title: Text("Settings"), message: Text("What do you want to do?"), buttons: [
+                .destructive(Text("Sign Out"), action: {
+                    print("handle sign out")
+                    vm.handleSignOut()
+                }),
                     .cancel()
-                ])
-            }
-            .fullScreenCover(isPresented: $vm.isUserCurrentlyLoggedOut, onDismiss: nil) {
-                LoginView(didCompleteLoginProcess: {
-                    self.vm.isUserCurrentlyLoggedOut = false
-                    self.vm.fetchCurrentUser()
-                })
-            }
+            ])
+        }
+        .fullScreenCover(isPresented: $vm.isUserCurrentlyLoggedOut, onDismiss: nil) {
+            LoginView(didCompleteLoginProcess: {
+                self.vm.isUserCurrentlyLoggedOut = false
+                self.vm.fetchCurrentUser()
+            })
+        }
     }
     
-    
-    private var messagesView: some View{
-        ScrollView{
-            ForEach (0..<10,id:\.self){ num in
-                VStack{
-                    HStack(spacing: 16){
+    private var messagesView: some View {
+        ScrollView {
+            ForEach(0..<10, id: \.self) { num in
+                VStack {
+                    HStack(spacing: 16) {
                         Image(systemName: "person.fill")
-                            .font(.system(size:32))
-                            .padding()
+                            .font(.system(size: 32))
+                            .padding(8)
                             .overlay(RoundedRectangle(cornerRadius: 44)
                                         .stroke(Color(.label), lineWidth: 1)
                             )
                         
                         
-                        
-                        VStack(alignment: .leading){
+                        VStack(alignment: .leading) {
                             Text("Username")
                                 .font(.system(size: 16, weight: .bold))
                             Text("Message sent to user")
@@ -174,34 +161,40 @@ struct MainMessagesView: View {
                         .padding(.vertical, 8)
                 }.padding(.horizontal)
                 
-            }.padding(.bottom, 60)
+            }.padding(.bottom, 50)
         }
     }
     
-    private var newMessageButton: some View{
+    @State var shouldShowNewMessageScreen = false
+    
+    private var newMessageButton: some View {
         Button {
-            
+            shouldShowNewMessageScreen.toggle()
         } label: {
-            HStack{
+            HStack {
                 Spacer()
                 Text("+ New Message")
-                    .font(.system(size: 16, weight:.bold))
+                    .font(.system(size: 16, weight: .bold))
                 Spacer()
             }
             .foregroundColor(.white)
             .padding(.vertical)
-            .background(Color.blue)
-            .cornerRadius(32)
-            .padding(.horizontal)
-            .shadow(radius: 15)
+                .background(Color.blue)
+                .cornerRadius(32)
+                .padding(.horizontal)
+                .shadow(radius: 15)
+        }.fullScreenCover(isPresented: $shouldShowNewMessageScreen) {
+            CreateNewMessageView()
         }
+
     }
 }
 
 struct MainMessagesView_Previews: PreviewProvider {
     static var previews: some View {
         MainMessagesView()
-        MainMessagesView()
             .preferredColorScheme(.dark)
+        
+        MainMessagesView()
     }
 }
